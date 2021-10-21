4a9c65b4901e77942190cdfcd303c0f988d48150
KenanOztrk <kenankenan006@gmail.com>
2021-10-21 15:48:44 +0300

    Add project files.

---
+++ KenanOzturk/Controllers/IletisimController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KenanOzturk.Controllers
+{
+    public class IletisimController : Controller
+    {
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
