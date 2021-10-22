cd8d73178abbd13b4ebb813e3411b1dd6085e39e
KenanOztrk <kenankenan006@gmail.com>
2021-10-22 11:34:33 +0300

    contact page

--- KenanOzturk/Controllers/IletisimController.cs
+++ KenanOzturk/Controllers/IletisimController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KenanOzturk.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace KenanOzturk.Controllers
@@ -10,6 +13,37 @@ namespace KenanOzturk.Controllers
     {
         public IActionResult Iletisim()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Iletisim(Email model)
+        {
+            MailMessage mailim = new MailMessage();
+            mailim.To.Add("example@gmail.com");
+            mailim.From = new MailAddress("example@gmail.com");
+            mailim.Subject = "Contact me sayfasından mesaj var." + model.Baslik;
+            mailim.Body = "Sayın yetkili," + model.AdSoyad + "kişisinden mesaj aldınız. <br>" + model.Icerik;
+            mailim.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential("example@gmail.com", "your password");
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+
+            try
+            {
+                smtp.Send(mailim);
+                TempData["Message"] = "Mesajınız iletilmiştir. E n kısa zamanda dönüş sağlanacaktır.";
+
+            }
+            catch (Exception ex)
+            {
+
+                TempData["Message"] = "Mesaj gönderilememiştir hata nedeni:" + ex.Message;
+            }
+
+
             return View();
         }
     }
--- KenanOzturk/Models/Email.cs
+++ KenanOzturk/Models/Email.cs
@@ -7,5 +7,8 @@ namespace KenanOzturk.Models
 {
     public class Email
     {
+        public string Baslik { get; set; }
+        public string Icerik { get; set; }
+        public string AdSoyad { get; set; }
     }
 }
